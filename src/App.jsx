3eecf6bfe0d334c6d7ae5daf702705b84d7d442d
Doniyor-s1mple <139375545+Doniyor-s1mple@users.noreply.github.com>
2023-12-02 16:30:27 +0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,40 @@
 import React from 'react'
-import FirstComponent from './Components/FirstComponent'
 import { createContext } from 'react'
-import { useState } from 'react'
 import ModalComponent from './ModalComponent'
+import { useReducer } from 'react'
 export const TextContext = createContext()
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "setActive":
+            return { ...state, active: !state.active }
+            break
+        case "setColor":
+            return { ...state, color: action.color }
+            break
+    }
+}
 
 const App = () => {
 
-    const [active, setActive] = useState(false)
-    const [color, setColor] = useState('white')
-
+    const [state, dispatch] = useReducer(reducer, {
+        active: false, color: 'white',
+    })
 
     const toggle = () => {
-        setActive(prev => !prev)
+        dispatch({
+            type: 'setActive'
+        })
+    }
+
+    var OrqaFon = (item) => {
+        dispatch({
+            type: 'setColor',
+            color: item
+        })
     }
     return (
-        <TextContext.Provider value={{ active, color, setColor, setActive }}>
+        <TextContext.Provider value={{ active: state.active, color: state.color, setColor: OrqaFon, setActive: () => { } }}>
             <div className='container my-5 p-3'>
                 <button className='btn btn-success btn-sm' onClick={toggle}>ActiveToggle</button>
 
